b8e9e59942f1e4a57263afede07a451f664f63f1
Your Name <you@example.com>
2024-02-28 17:19:29 +0300

    rad

--- app/src/main/java/com/raywenderlich/android/lab1/screens/RowSreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/RowSreen.kt
@@ -2,20 +2,44 @@ package com.raywenderlich.android.lab1.screens
 
 import androidx.compose.runtime.Composable
 import com.raywenderlich.android.lab1.R
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.sp
 import com.raywenderlich.android.lab1.router.Screen
 
-val THREE_ELEMENT_LIST = listOf(R.string.first, R.string.second, R.string.third)
+val THREE_ELEMENTS_LIST = listOf(R.string.first, R.string.second, R.string.third)
+
 
 @Composable
 fun RowScreen(){
     MyRow()
 
-    BackButtonHandler {
-    FundamentalsRouter.navigateTo(Screen.Navigation)
+    BackButtonHandler{
+        FundamentalsRouter.navigateTo(Screen.Navigation)
     }
-}
-fun MyRow(){
 
+}
+
+@Composable
+fun MyRow() {
+    Row(verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier.fillMaxSize()){
+
+        THREE_ELEMENTS_LIST.forEach { textResId ->
+            Text(
+                text = stringResource(id = textResId),
+                fontSize = 18.sp
+            )
+        }
+
+
+    }
 }
